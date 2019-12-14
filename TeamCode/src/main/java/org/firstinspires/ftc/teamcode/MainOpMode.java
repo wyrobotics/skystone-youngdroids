73f7d28cc30c5a54c6964c2e7e9a55084ea09bf3
Juan <jdlandi@cps.edu>
2019-12-14 12:05:52 -0600

    Cleanup and more potential bug fixes
    Signed-off-by: Juan <jdlandi@cps.edu>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainOpMode.java
@@ -5,7 +5,6 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
-
 import org.firstinspires.ftc.robotcore.internal.ftdi.eeprom.FT_EEPROM_232H;
 
 @TeleOp(name="MainOpModeNew", group="Robot")
@@ -18,102 +17,90 @@ public class MainOpMode extends LinearOpMode {
     @Override
     public void runOpMode() {
         //Robot.init(hardwareMap);
-        Drive.init(hardwareMap);
-        DcMotor InL, InR;
-        Servo PlateGrabberL, PlateGrabberR, Grabber;
+        Drive.init(hardwareMap); // Maps our hardware
+        Drive.zeroALL(); // Sets all of our positions and powers to 0
 
         telemetry.addData("say", "before opmode");
         telemetry.update();
+
         waitForStart();
 
-        boolean aPressed = false;
-        boolean yPressed = false;
-        boolean xPressed = true;
-        boolean bPressed;
-        boolean dPadUp = false;
-        boolean dPadDown = false;
-        boolean rightTrigger = false, leftTrigger = false;
+        boolean aPressed = false, bPressed = false; // Variables for Buttons
+        boolean yPressed = false, xPressed = false;
+
+        boolean dPadUp = false, dPadDown = false; // Variables for dPad
+
+        boolean rightTrigger = false, leftTrigger = false; // Variables for our triggers
 
         while (opModeIsActive()) {
-            if (rightTrigger) {
+
+            if (rightTrigger) { // Strafes if either Trigger is pressed
                 Drive.DriveTrain(5);
             } else if (leftTrigger) {
                 Drive.DriveTrain(-5);
             }
-            else {
+            else { // Drives normally otherwise
                 Drive.DriveTrain(gamepad1.left_stick_x,gamepad1.left_stick_y,gamepad1.right_stick_x);
             }
 
-            Drive.SetMotorPower();
+            if(bPressed) { // Lifts release when pressed
+                Drive.SetMotorPower();
+                Drive.releaseInPos = .5;
+            }
 
-            if(aPressed){
-                Drive.InLPower = 1;
-                Drive.InRPower = 1;
+            if(aPressed){ // Intake power
+                Drive.InLPower = Drive.InRPower = 1;
             } else if (xPressed){
-                Drive.InLPower = 0;
-                Drive.InRPower = 0;
+                Drive.InLPower = Drive.InRPower = 0;
             } else if (yPressed){
-                Drive.InLPower = -1;
-                Drive.InRPower = -1;
-            }
-            if(dPadUp){
-                Drive.PlateGrabLPos = .1;
-                Drive.PlateGrabRPos = .1;
-            } else if (dPadDown){
-                Drive.PlateGrabLPos = .6;
-                Drive.PlateGrabRPos = .6;
-            }
-            if(gamepad1.left_bumper){
-                Drive.PlateGrabLPos += .1;
-                Drive.PlateGrabRPos += .1;
-            } else if(gamepad1.right_bumper){
-                Drive.PlateGrabLPos -= .1;
-                Drive.PlateGrabRPos -= .1;
+                Drive.InLPower  = Drive.InRPower = -1;
             }
 
-            if(gamepad1.dpad_up){
+            if(dPadUp){ // PlateGrabber power
+                Drive.PlateGrabLPos = .45; Drive.PlateGrabRPos = .45;
+            } else if (dPadDown){
+                Drive.PlateGrabLPos = 0; Drive.PlateGrabRPos = 0;
+            }
+
+
+
+            if(gamepad1.dpad_up){ // If One of them is pressed, the other is off
                 dPadUp = true;
                 dPadDown = false;
             } else if (gamepad1.dpad_down){
                 dPadUp = false;
                 dPadDown = true;
-            } else {
+            } else { // Else, turn both off
                 dPadUp = dPadDown = false;
             }
-            if(gamepad1.a){
+
+            if(gamepad1.a){ // Same mechanism here, but with 3 values
                 aPressed = true;
-                xPressed = false;
-                yPressed = false;
+                xPressed = yPressed = false;
             } else if (gamepad1.x){
-                aPressed = false;
                 xPressed = true;
-                yPressed = false;
+                aPressed = yPressed = false;
             } else if (gamepad1.y){
-                aPressed = false;
-                xPressed = false;
                 yPressed = true;
+                aPressed = xPressed = false;
             } else {
                 aPressed = xPressed = yPressed = false;
             }
 
             if (gamepad1.left_trigger > 0) { // Unsure how Triggers work, might give negative value instead of positive
-                leftTrigger = true;
-                rightTrigger = false;
+                rightTrigger = false; leftTrigger = true;
             } else if (gamepad1.right_trigger > 0) {
-                rightTrigger = true;
-                leftTrigger = false;
+                rightTrigger = true; leftTrigger = false;
             } else {
                 rightTrigger = leftTrigger = false;
             }
 
-            //TODO: Something to use strafe (Not required)
-            //we have four motors for the drive base, and two at the front of the robot for intake. We have two rev servos at the back that control the clamps for pulling out the building spot. we are planning on adding a servo in the middle of the robot too hold back the intake at the beginning of the game. i think we should have that rotate up to release the intake in auton, or if it doesnt get released during auton we should have the X button release it
-            //for the servos maybe we should use the dpad up and down and for the intake um idk maybe the left and right bumper?
 
-            telemetry.addData("LFMotorPower",Drive.LFWheelPower);
-            telemetry.addData("LBMotorPower",Drive.LBWheelPower);
-            telemetry.addData("RFMotorPower",Drive.RFWheelPower);
-            telemetry.addData("RBMotorPower",Drive.RBWheelPower);
+            // Places info on our phone for our reference
+            telemetry.addData("LFMotorPower",Drive.flWheelPower);
+            telemetry.addData("LBMotorPower",Drive.blWheelPower);
+            telemetry.addData("RFMotorPower",Drive.frWheelPower);
+            telemetry.addData("RBMotorPower",Drive.brWheelPower);
             //telemetry.addData("Grabber Open?",Robot.GrabOpen);
             telemetry.addData("LF Position: ", Drive.fl.getCurrentPosition());
             telemetry.addData("LR Position: ", Drive.bl.getCurrentPosition());
@@ -125,15 +112,8 @@ public class MainOpMode extends LinearOpMode {
             telemetry.addData("GrabR Current Pos: ", Drive.PlateGrabR.getPosition());
             telemetry.addData("GrabR Target Pos: ", Drive.PlateGrabRPos);
             telemetry.update();
-            //
-
         }
-
-
-
-
-
-
+        Drive.zeroALL(); // Sets everything to zero after it ends
     }
     public double AverageRotation(){ //Averages the number of rotations that the 4 wheels have
         return (Drive.fl.getCurrentPosition() + Drive.fr.getCurrentPosition() + Drive.bl.getCurrentPosition() + Drive.br.getCurrentPosition()) / 4;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -10,12 +10,12 @@ public class MecanumDrive{
 
     public void init(HardwareMap HM) {
         fl = HM.dcMotor.get("fl"); bl = HM.dcMotor.get("bl"); // map the Front wheels
-        fr = HM.dcMotor.get("fr"); br = HM.dcMotor.get("br"); // Map the Back wheels
-        InL = HM.dcMotor.get("InL"); InR = HM.dcMotor.get("InR");
-        releaseIn = HM.servo.get("releaseIn");
-        PlateGrabL = HM.servo.get("PlateGrabL"); PlateGrabR = HM.servo.get("PlateGrabR");
+        fr = HM.dcMotor.get("fr"); br = HM.dcMotor.get("br"); // map the Back wheels
+        InL = HM.dcMotor.get("InL"); InR = HM.dcMotor.get("InR"); // map the intake
+        releaseIn = HM.servo.get("releaseIn"); // map release
+        PlateGrabL = HM.servo.get("PlateGrabL"); PlateGrabR = HM.servo.get("PlateGrabR"); // map plate grabbers
 
-
+        // Wheel initializers
         fr.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER); br.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         fl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER); bl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         fr.setMode(DcMotor.RunMode.RUN_USING_ENCODER); br.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
@@ -27,13 +27,18 @@ public class MecanumDrive{
         fl.setDirection(DcMotorSimple.Direction.REVERSE); bl.setDirection(DcMotorSimple.Direction.REVERSE);// reverse the left wheels direction
         InL.setDirection(DcMotorSimple.Direction.REVERSE);
         PlateGrabL.setDirection(Servo.Direction.REVERSE);
-        PlateGrabR.setDirection(Servo.Direction.FORWARD);
+        //PlateGrabR.setDirection(Servo.Direction.FORWARD);
     }
-
+    public void zeroALL() { // Zeros all the functions
+        flWheelPower = blWheelPower = frWheelPower = brWheelPower = 0;
+        releaseInPos = PlateGrabLPos = PlateGrabRPos = InRPower = InLPower = 0;
+    }
     DcMotor fl, bl, fr, br; // 4 Drive Motors
     DcMotor InL, InR; // Intake Motors
     Servo releaseIn, PlateGrabL, PlateGrabR; // Plate Servos + Release
-    double LFWheelPower, LBWheelPower, RFWheelPower, RBWheelPower, releaseInPos, PlateGrabLPos, PlateGrabRPos, InLPower, InRPower; // Power of Wheels
+
+    double flWheelPower, blWheelPower, frWheelPower, brWheelPower; // Power of Wheels
+    double releaseInPos, PlateGrabLPos, PlateGrabRPos, InLPower, InRPower; // release, plateGrabbers, and intake powers
 
 
     /*public void DriveTrain(double left_stick_x,double left_stick_y, double right_stick_x) {// the math for the mecanum wheel
@@ -48,19 +53,20 @@ public class MecanumDrive{
     }
      */
     public void DriveTrain(double left_stick_x,double left_stick_y, double right_stick_x) {// the math for the mecanum wheel
-        LFWheelPower = -left_stick_y + right_stick_x + left_stick_x;
-        LBWheelPower = -left_stick_y + right_stick_x - left_stick_x;
-        RFWheelPower = -left_stick_y - right_stick_x - left_stick_x;
-        RBWheelPower = -left_stick_y - right_stick_x + left_stick_x;
-    }
-    public void DriveTrain(double x) {// the math for the mecanum wheel
-        LFWheelPower = RFWheelPower = x;
-        LBWheelPower = RBWheelPower = -x;
+        flWheelPower = -left_stick_y + right_stick_x + left_stick_x;
+        blWheelPower = -left_stick_y + right_stick_x - left_stick_x;
+        frWheelPower = -left_stick_y - right_stick_x - left_stick_x;
+        brWheelPower = -left_stick_y - right_stick_x + left_stick_x;
     }
 
-    public void SetMotorPower(){
-        fl.setPower(LFWheelPower); bl.setPower(LBWheelPower);// set the wheel power to what it should be
-        fr.setPower(RFWheelPower); br.setPower(RBWheelPower);
+    public void DriveTrain(double x) { //Strafing
+        flWheelPower = frWheelPower = x;
+        blWheelPower = brWheelPower = -x;
+    }
+
+    public void SetMotorPower(){ // Sets the power for ALL motors and servos
+        fl.setPower(flWheelPower); bl.setPower(blWheelPower);
+        fr.setPower(frWheelPower); br.setPower(brWheelPower);
 
         releaseIn.setPosition(releaseInPos);
         PlateGrabL.setPosition(PlateGrabLPos); PlateGrabR.setPosition(PlateGrabRPos);
