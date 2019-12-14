bc7b302819ce594f36f15539eed02c51887e5a85
twerner1 <twerner@cps.edu>
2019-12-13 21:57:02 -0600

    Finished basic MainOpMode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainOpMode.java
@@ -6,6 +6,8 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.robotcore.internal.ftdi.eeprom.FT_EEPROM_232H;
+
 @TeleOp(name="MainOpMode", group="Robot")
 public class MainOpMode extends LinearOpMode {
 
@@ -16,7 +18,7 @@ public class MainOpMode extends LinearOpMode {
     private static double LFMotorPower = 0.0, LBMotorPower = 0.0, RFMotorPower = 0.0, RBMotorPower = 0.0;
 
     @Override
-    public void runOpMode() throws InterruptedException {
+    public void runOpMode() {
         //Robot.init(hardwareMap);
         Drive.init(hardwareMap);
         DcMotor InL, InR;
@@ -26,27 +28,64 @@ public class MainOpMode extends LinearOpMode {
         telemetry.update();
         waitForStart();
 
-        boolean xPressed, yPressed, aPressed, bPressed, dPadUp, dPadDown, rightTrigger, leftTrigger;
+        boolean aPressed = false;
+        boolean yPressed = false;
+        boolean xPressed = true;
+        boolean bPressed;
+        boolean dPadUp = true;
+        boolean dPadDown = false;
+        boolean rightTrigger, leftTrigger;
 
         while (opModeIsActive()) {
             Drive.DriveTrain(gamepad1.left_stick_x,gamepad1.left_stick_y,gamepad1.right_stick_x);
             Drive.SetMotorPower();
 
-            if (!this.gamepad1.dpad_up == this.gamepad1.dpad_down) { //TODO: Copy and paste code,
-                                                                    // but make the robot strafe instead of moving the release
-                if (!this.gamepad1.dpad_up) {
-                    Drive.releaseInPower = 0.2;
-                }
-                else {
-                    Drive.releaseInPower = -0.2;
-                }
-            } else {
-                Drive.releaseInPower = 0;
+            if(aPressed){
+                Drive.InLPower = .5;
+                Drive.InRPower = .5;
+            } else if (xPressed){
+                Drive.InLPower = 0;
+                Drive.InRPower = 0;
+            } else if (yPressed){
+                Drive.InLPower = -.5;
+                Drive.InRPower = -.5;
             }
-            //TODO: Make the X button turn on/off the intakes
-            //TODO: Something to use strafe (Not required)
-            //TODO: Controls for the Plate Grabbers
+            if(dPadUp){
+                Drive.PlateGrabLPos = 1;
+                Drive.PlateGrabRPos = 1;
+            } else if (dPadDown){
+                Drive.PlateGrabLPos = 0;
+                Drive.PlateGrabRPos = 0;
+            }
 
+
+
+            if(gamepad1.dpad_up){
+                dPadUp = true;
+                dPadDown = false;
+            } else if (gamepad1.dpad_down){
+                dPadUp = false;
+                dPadDown = true;
+            }
+            if(gamepad1.a){
+                aPressed = true;
+                xPressed = false;
+                yPressed = false;
+            } else if (gamepad1.x){
+                aPressed = false;
+                xPressed = true;
+                yPressed = false;
+            } else if (gamepad1.y){
+                aPressed = false;
+                xPressed = false;
+                yPressed = true;
+            }
+
+
+
+            //TODO: Something to use strafe (Not required)
+            //we have four motors for the drive base, and two at the front of the robot for intake. We have two rev servos at the back that control the clamps for pulling out the building spot. we are planning on adding a servo in the middle of the robot too hold back the intake at the beginning of the game. i think we should have that rotate up to release the intake in auton, or if it doesnt get released during auton we should have the X button release it
+            //for the servos maybe we should use the dpad up and down and for the intake um idk maybe the left and right bumper?
 
             telemetry.addData("LFMotorPower",Drive.LFWheelPower);
             telemetry.addData("LBMotorPower",Drive.LBWheelPower);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -1,10 +1,9 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.GyroSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 
 public class MecanumDrive{
@@ -13,8 +12,8 @@ public class MecanumDrive{
         fl = HM.dcMotor.get("fl"); bl = HM.dcMotor.get("bl"); // map the Front wheels
         fr = HM.dcMotor.get("fr"); br = HM.dcMotor.get("br"); // Map the Back wheels
         InL = HM.dcMotor.get("InL"); InR = HM.dcMotor.get("InR");
-        releaseIn = HM.crservo.get("releaseIn");
-        PlateGrabL = HM.crservo.get("PlateGrabL"); PlateGrabR = HM.crservo.get("PlateGrabR");
+        releaseIn = HM.servo.get("releaseIn");
+        PlateGrabL = HM.servo.get("PlateGrabL"); PlateGrabR = HM.servo.get("PlateGrabR");
 
 
         fr.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER); br.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -25,13 +24,14 @@ public class MecanumDrive{
         fr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE); br.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         fl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE); bl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        fl.setDirection(DcMotorSimple.Direction.REVERSE); fl.setDirection(DcMotorSimple.Direction.REVERSE);// reverse the left wheels direction
+        fr.setDirection(DcMotorSimple.Direction.REVERSE); br.setDirection(DcMotorSimple.Direction.REVERSE);// reverse the left wheels direction
+        InR.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
     DcMotor fl, bl, fr, br; // 4 Drive Motors
     DcMotor InL, InR; // Intake Motors
-    CRServo releaseIn, PlateGrabL, PlateGrabR; // Plate Servos + Release
-    double LFWheelPower, LBWheelPower, RFWheelPower, RBWheelPower, releaseInPower, PlateGrabLPower, PlateGrabRPower, InLPower, InRPower; // Power of Wheels
+    Servo releaseIn, PlateGrabL, PlateGrabR; // Plate Servos + Release
+    double LFWheelPower, LBWheelPower, RFWheelPower, RBWheelPower, releaseInPos, PlateGrabLPos, PlateGrabRPos, InLPower, InRPower; // Power of Wheels
 
 
     /*public void DriveTrain(double left_stick_x,double left_stick_y, double right_stick_x) {// the math for the mecanum wheel
@@ -56,8 +56,8 @@ public class MecanumDrive{
         fl.setPower(LFWheelPower); bl.setPower(LBWheelPower);// set the wheel power to what it should be
         fr.setPower(RFWheelPower); br.setPower(RBWheelPower);
 
-        releaseIn.setPower(releaseInPower);
-        PlateGrabL.setPower(PlateGrabLPower); PlateGrabR.setPower(PlateGrabRPower);
+        releaseIn.setPosition(releaseInPos);
+        PlateGrabL.setPosition(PlateGrabLPos); PlateGrabR.setPosition(PlateGrabRPos);
         InL.setPower(InLPower); InR.setPower(InLPower);
         // The strafing makes the front go forward and back go backward when controller goes to the right
     }
