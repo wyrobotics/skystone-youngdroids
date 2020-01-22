edc4a065a39cbabc924491401fb30d29cc65609c
Juan <jdlandi@cps.edu>
2020-01-21 21:27:47 -0600

    Motor/Servo test code complete
    Signed-off-by: Juan <jdlandi@cps.edu>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/FindValuesMotors.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/FindValuesMotors.java
@@ -8,7 +8,7 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.SkyStone.MecanumDrive;
 
-@TeleOp(name="FindValuesMotors2019", group = "SkyStone")
+@TeleOp(name="FindValuesMotors2019", group = "Test")
 public class FindValuesMotors extends LinearOpMode {
     
     MecanumDrive Drive = new MecanumDrive();
@@ -16,12 +16,13 @@ public class FindValuesMotors extends LinearOpMode {
 
     @Override
     public void runOpMode() {
-        //Robot.init(hardwareMap);
+
         Drive.init(hardwareMap);
-        telemetry.addData("say", "before opmode");
+        telemetry.addData("say", "before testmode");
         telemetry.update();
         waitForStart();
-        //Drive.releaseInPos = 0.5;
+
+
         boolean aPressed = false;
         boolean yPressed = false;
         boolean xPressed = false;
@@ -30,9 +31,12 @@ public class FindValuesMotors extends LinearOpMode {
         boolean dPadDown = true;
         boolean rightBumper = false, leftBumper = false;
         boolean dPadLeft = false, dPadRight = false;
+        boolean rightTrigger = false, leftTrigger = false;
+
 
         while (opModeIsActive()) {
-            //Driving
+
+            // Controller Stuff
             if (gamepad1.right_bumper) {
                 rightBumper = true;
                 leftBumper = false;
@@ -42,34 +46,15 @@ public class FindValuesMotors extends LinearOpMode {
             } else {
                 rightBumper = leftBumper = false;
             }
-            if (rightBumper) {
-                Drive.DriveTrain(5);
-            } else if (leftBumper) {
-                Drive.DriveTrain(-5);
-            } else {
-                Drive.DriveTrain(gamepad1.left_stick_x,gamepad1.left_stick_y,gamepad1.right_stick_x);
-            }
-
-
-
-
-            Drive.SetMotorPower();
-            //grabber
             if(gamepad1.dpad_up){
                 dPadUp = true;
                 dPadDown = false;
             } else if (gamepad1.dpad_down){
                 dPadUp = false;
                 dPadDown = true;
+            } else {
+                dPadUp = dPadDown = false;
             }
-            if(dPadDown){
-                Drive.PlateGrabLPos = .6;
-                Drive.PlateGrabRPos = .6;
-            } else if (dPadUp){
-                Drive.PlateGrabLPos = .1;
-                Drive.PlateGrabRPos = .1;
-            }
-            //grabber fine adjustment
             if(gamepad1.dpad_left){
                 dPadLeft = true;
                 dPadRight = false;
@@ -79,73 +64,84 @@ public class FindValuesMotors extends LinearOpMode {
             } else {
                 dPadRight = dPadLeft = false;
             }
-            if(dPadLeft){
-                Drive.releaseInPos += 5;
-            } else if(dPadRight){
-                Drive.releaseInPos -= 5;
-            }
-            //intake all controls
             if(gamepad1.a){
                 aPressed = true;
                 xPressed = false;
                 yPressed = false;
+                bPressed = false;
             } else if (gamepad1.x){
                 aPressed = false;
                 xPressed = true;
                 yPressed = false;
+                bPressed = false;
             } else if (gamepad1.y){
                 aPressed = false;
                 xPressed = false;
                 yPressed = true;
+                bPressed = false;
+            } else if (gamepad1.b){
+                aPressed = false;
+                xPressed = false;
+                yPressed = false;
+                bPressed = true;
+            } else {
+                aPressed = xPressed = yPressed = bPressed = false;
             }
-            if(aPressed){
-                Drive.InLPower = 5;
-                Drive.InRPower = 5;
-            } else if (xPressed){
-                Drive.InLPower = 0;
-                Drive.InRPower = 0;
-            } else if (yPressed){
-                Drive.InLPower = -.5;
-                Drive.InRPower = -.5;
+            if (gamepad1.left_trigger > 0.8) {
+                leftTrigger = true;
+                rightTrigger = false;
+            } else if (gamepad1.right_trigger > 0.8) {
+                leftTrigger = false;
+                rightTrigger = true;
+            } else {
+                leftTrigger = rightTrigger = false;
             }
 
-            if(gamepad1.b) {
-                Drive.releaseInPos -= 1;
+            // Motor and servo Stuff
+            if (rightBumper) {
+                Drive.LifterPower = 1;
+            } else if (leftBumper) {
+                Drive.LifterPower = -1;
+            } else {
+                Drive.LifterPower = 0;
+            }
+            if  (leftTrigger) {
+                Drive.GrabberPos += .1;
+            } else if (rightTrigger) {
+                Drive.GrabberPos -= .1;
+            }
+            if (dPadUp) {
+                Drive.PlateGrabLPos += .1;
+                Drive.PlateGrabRPos += .1;
+            } else if (dPadDown) {
+                Drive.PlateGrabLPos -= .1;
+                Drive.PlateGrabRPos -= .1;
+            }
+            if (xPressed) {
+                Drive.releaseInPos -= .1;
+            } else if (bPressed) {
+                Drive.releaseInPos += .1;
+            }
+            if (yPressed) {
+                Drive.InLPower += 1;
+                Drive.InRPower += 1;
+            } else if (xPressed) {
+                Drive.InRPower -= 1;
+                Drive.InLPower += 1;
             }
 
+            Drive.SetMotorPower();
 
+            // Phone Data collecty Stuff
+            telemetry.addData("InLPower: ",Drive.InLPower);
+            telemetry.addData("InRPower: ",Drive.InRPower);
+            telemetry.addData("PlateGrabLPos: ",Drive.PlateGrabLPos);
+            telemetry.addData("PlateGrabRPos: ",Drive.PlateGrabRPos);
+            telemetry.addData("LifterPower: ",Drive.LifterPower);
+            telemetry.addData("GrabberPos: ",Drive.GrabberPos);
+            telemetry.addData("releaseInPos: ",Drive.releaseInPos);
 
-
-            //TODO: Something to use strafe (Not required)
-            //we have four motors for the drive base, and two at the front of the robot for intake. We have two rev servos at the back that control the clamps for pulling out the building spot. we are planning on adding a servo in the middle of the robot too hold back the intake at the beginning of the game. i think we should have that rotate up to release the intake in auton, or if it doesnt get released during auton we should have the X button release it
-            //for the servos maybe we should use the dpad up and down and for the intake um idk maybe the left and right bumper?
-
-            telemetry.addData("LFMotorPower",Drive.LFWheelPower);
-            telemetry.addData("LBMotorPower",Drive.LBWheelPower);
-            telemetry.addData("RFMotorPower",Drive.RFWheelPower);
-            telemetry.addData("RBMotorPower",Drive.RBWheelPower);
-            //telemetry.addData("Grabber Open?",Robot.GrabOpen);
-            telemetry.addData("LF Position: ", Drive.fl.getCurrentPosition());
-            telemetry.addData("LR Position: ", Drive.bl.getCurrentPosition());
-            telemetry.addData("RF Position: ", Drive.fr.getCurrentPosition());
-            telemetry.addData("RB Position: ", Drive.br.getCurrentPosition());
-            telemetry.addData("Average Pos: ", AverageRotation());
-            telemetry.addData("GrabL Current Pos: ", Drive.PlateGrabL.getPosition());
-            telemetry.addData("GrabL Target Pos: ", Drive.PlateGrabLPos);
-            telemetry.addData("GrabR Current Pos: ", Drive.PlateGrabR.getPosition());
-            telemetry.addData("GrabR Target Pos: ", Drive.PlateGrabRPos);
             telemetry.update();
-            //
-
         }
-
-
-
-
-
-
-    }
-    public double AverageRotation(){ //Averages the number of rotations that the 4 wheels have
-        return (Drive.fl.getCurrentPosition() + Drive.fr.getCurrentPosition() + Drive.bl.getCurrentPosition() + Drive.br.getCurrentPosition()) / 4;
     }
 }
