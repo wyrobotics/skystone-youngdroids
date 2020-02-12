f79050454d4cf724c01d4b2ce8bfcd363da83ab8
Juan <jdlandi@cps.edu>
2020-02-12 17:21:23 -0600

    Fixed A LOT of issues
    Signed-off-by: Juan <jdlandi@cps.edu>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ForOffSeason/FindValuesMotorsEXPO.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ForOffSeason/FindValuesMotorsEXPO.java
@@ -7,19 +7,20 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@TeleOp(name = "ExpoDrive")
-public class ExpoDrive extends LinearOpMode {
+@TeleOp(name = "ExpoDrive Motor Vals")
+public class FindValuesMotorsEXPO extends LinearOpMode {
 
     public DcMotor fl, bl, fr, br; // 4 Drive Motors
     public Servo Grabber;
     public DcMotor Lifter;
     public DigitalChannel tSensor;
     public double LFWheelPower, LBWheelPower, RFWheelPower, RBWheelPower; // Power/Position of m/s
-    public double GrabberOpenPos = 0.819;
-    public double GrabberClosePos = 1.0;
+    public double GrabberOpenPos = 0.180;
+    public double GrabberClosePos = 0.0020;
     public double LiftBottom = 0;
     public double LiftTop = 2700;
 
+    public double grabberPos = 0;
 
 
     @Override
@@ -28,8 +29,6 @@ public class ExpoDrive extends LinearOpMode {
         fl = hardwareMap.dcMotor.get("fl"); bl = hardwareMap.dcMotor.get("bl"); // Maps all our motors/servos
         fr = hardwareMap.dcMotor.get("fr"); br = hardwareMap.dcMotor.get("br");
         Lifter = hardwareMap.dcMotor.get("Lifter"); Grabber = hardwareMap.servo.get("Grabber");
-
-        Grabber.setPosition(GrabberOpenPos);
 
         tSensor = hardwareMap.get(DigitalChannel.class,"tSensor");
 
@@ -44,23 +43,34 @@ public class ExpoDrive extends LinearOpMode {
         fr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE); br.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         fl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE); bl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
+        Grabber.setDirection(Servo.Direction.REVERSE);
 
         fl.setDirection(DcMotorSimple.Direction.REVERSE); bl.setDirection(DcMotorSimple.Direction.REVERSE);
         tSensor.setMode(DigitalChannel.Mode.INPUT);
 
-        Grabber.setPosition(GrabberOpenPos);
-
+        //Grabber.setPosition(GrabberOpenPos);
+        Grabber.setPosition(GrabberClosePos);
         while (tSensor.getState()) {
             Lifter.setPower(-1);
         }
         try {
-            sleep(650);
+            sleep(250);
         } catch (Exception e){
             e.getStackTrace();
         }
         Lifter.setPower(0);
+        try {
+            sleep(250);
+        } catch (Exception e){
+            e.getStackTrace();
+        }
+        Lifter.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        Lifter.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        Lifter.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         waitForStart();
+
+        Grabber.setPosition(GrabberOpenPos);
 
         while(opModeIsActive()){
 
@@ -80,13 +90,22 @@ public class ExpoDrive extends LinearOpMode {
                 Lifter.setPower(0);
             }
             if(gamepad1.b){
-                Grabber.setPosition(GrabberOpenPos);
+                grabberPos += 0.001;
+                Grabber.setPosition(grabberPos);
             }
             if(gamepad1.x){
+                grabberPos -= 0.001;
+                Grabber.setPosition(grabberPos);
+            }
+            if(gamepad1.y) {
+                Grabber.setPosition(GrabberOpenPos);
+            }
+            if (gamepad1.a) {
                 Grabber.setPosition(GrabberClosePos);
             }
             telemetry.addData("Lift", Lifter.getCurrentPosition());
             telemetry.addData("tSensor", tSensor.getState());
+            telemetry.addData("grabber: ", grabberPos);
             telemetry.update();
         }
 
